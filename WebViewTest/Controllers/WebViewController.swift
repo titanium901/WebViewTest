8e421d8d78e855ed3463cdea8d8f148773ff7878
titanium901 <nemes901@hotmail.com>
2019-10-18 14:56:17 +0300

    hide logo when scroll

--- WebViewTest/Controllers/WebViewController.swift
+++ WebViewTest/Controllers/WebViewController.swift
@@ -15,14 +15,16 @@ class WebViewController: UIViewController {
     @IBOutlet weak var webView: WKWebView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var logoImage: UIImageView!
     
+    private var lastContentOffset: CGFloat = 10
     private var website = "https://new.faberlic.com/"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
         setupUrl()
-        
+        self.webView.scrollView.delegate = self
         
     }
     
@@ -83,7 +85,6 @@ extension WebViewController: WKNavigationDelegate {
             let removeElementClassScript = "var element = document.getElementsByClassName('\(className)') [0]; element.parentNode.removeChild(element);"
             webView.evaluateJavaScript(removeElementClassScript) { (response, error) in
                 debugPrint("Am here")
-                self.webView.scrollView.setContentOffset(.zero, animated: true)
             }
         }
         
@@ -97,5 +98,27 @@ extension WebViewController: WKNavigationDelegate {
         
     }
     
+}
+
+extension WebViewController: UIScrollViewDelegate {
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+          lastContentOffset = scrollView.contentOffset.y
+       }
+
+       func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(lastContentOffset)
+        print(scrollView.contentOffset.y)
+        if lastContentOffset > scrollView.contentOffset.y {
+               UIView.animate(withDuration: 0.25, animations: { [weak self] in
+                   self?.logoImage.alpha = 1.0
+                   self?.logoImage.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+               }, completion: nil)
+           } else if lastContentOffset < scrollView.contentOffset.y {
+               UIView.animate(withDuration: 0.25, animations: { [weak self] in
+                   self?.logoImage.alpha = 0
+                   self?.logoImage.transform = CGAffineTransform(scaleX: 1.3, y: 1.3)
+               }, completion: nil)
+           }
+       }
 }
 
